15190e777e9e88dc70ed2e27924ce2395c6c6f53
Vivek Mishra <vivekmishra22117@gmail.com>
2018-07-28 19:33:22 +0530

    Added project to repo

---
+++ src/main/java/com/vivek/domain/App.java
@@ -0,0 +1,11 @@
+package com.vivek.domain;
+
+/**
+ * Created by Vivek Kumar Mishra on 28/07/2018.
+ */
+public class App {
+
+    public void returnSomeThing(){
+        System.out.println("Done!");
+    }
+}
---
+++ src/test/java/com/vivek/domain/AppTest.java
@@ -0,0 +1,15 @@
+package com.vivek.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Vivek Kumar Mishra on 28/07/2018.
+ */
+public class AppTest {
+
+    @Test
+    public void testMe(){
+    }
+}
